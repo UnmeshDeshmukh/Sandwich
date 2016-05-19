a62911e5f2c2097154d5ee7f5b854d5b4c729d21
vinitgaikwad0810 <vinit.gaikwad10@gmail.com>
2016-05-19 01:22:47 -0700

    Mutex added

---
+++ omp_sync.h
@@ -0,0 +1,33 @@
+#include <omp.h>
+
+class Mutex {
+public:
+	Mutex() {
+		omp_init_lock(&_mutex);
+	}
+	~Mutex() {
+		omp_destroy_lock(&_mutex);
+	}
+	void lock() {
+		omp_set_lock(&_mutex);
+	}
+	void unlock() {
+		omp_unset_lock(&_mutex);
+	}
+
+	static Mutex* getMutex() {
+		if (mutexInstance != NULL) {
+			return mutexInstance;
+		} else {
+			mutexInstance = new Mutex;
+			return mutexInstance;
+		}
+	}
+
+private:
+	omp_lock_t _mutex;
+	static Mutex* mutexInstance;
+};
+
+Mutex* Mutex::mutexInstance;
+
